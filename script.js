c7baaedcca430ba5ed3915382e47c2090568d31d
João Victor Ribeiro <joao.leiterib19@gmail.com>
2023-02-02 15:55:32 -0300

    Finalização da responsividade

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let control = true;
 let control_2 = true;
 let dark = true;
+let companyControl = true;
+let featureControl = true;
 
 document.querySelector('.features').addEventListener('click', openBox1);
 document.querySelector('.company').addEventListener('click', openBox2);
 document.querySelector('.dark').addEventListener('click', darkMode);
+document.querySelector('.m_features_op .dark').addEventListener('click', darkMode);
+document.querySelector('.m_company').addEventListener('click', companyMobile);
+document.querySelector('.m_features').addEventListener('click', featuresMobile);
+
+document.querySelector('.icon').addEventListener('click', () =>{
+    document.querySelector('.menu').style.display = 'flex';
+    document.querySelector('.side_menu').style.display = 'block';
+});
+document.querySelector('.m_header span').addEventListener('click', () =>{
+    document.querySelector('.menu').style.display = 'none';
+    document.querySelector('.side_menu').style.display = 'none';
+});
+
 
 function openBox1(){
 
@@ -41,7 +56,9 @@ function darkMode(){
         document.querySelector('p').style.color = 'hsl(0, 0%, 90%)';
         document.querySelector('h1').style.color = 'hsl(0, 0%, 100%)';
         document.querySelector('.dark span').innerHTML = 'ON';
-
+        document.querySelector('.m_features_op .dark span').innerHTML = 'ON';
+        document.querySelector('.menu').style.backgroundColor = 'hsl(0, 0%, 21%)';
+        document.querySelector('.menu').style.color = 'hsl(0, 0%, 90%)';
     }else{
         dark = true;
         document.querySelector('body').style.backgroundColor = 'hsl(0, 0%, 98%)';
@@ -51,5 +68,33 @@ function darkMode(){
         document.querySelector('p').style.color = 'hsl(0, 0%, 41%)';
         document.querySelector('h1').style.color = 'hsl(0, 0%, 8%)';
         document.querySelector('.dark span').innerHTML = 'OFF';
+        document.querySelector('.m_features_op .dark span').innerHTML = 'OFF';
+        document.querySelector('.menu').style.backgroundColor = 'hsl(0, 0%, 93%)';
+        document.querySelector('.menu').style.color = 'hsl(0, 0%, 41%)';
+    }
+}
+
+
+function featuresMobile(){
+    if(featureControl){
+        document.querySelector('.m_features_op').style.display = 'flex';
+        document.querySelector('.m_features img').setAttribute('src', 'images/icon-arrow-up.svg');
+        featureControl = false;
+    }else{ 
+        document.querySelector('.m_features_op').style.display = 'none';
+        document.querySelector('.m_features img').setAttribute('src', 'images/icon-arrow-down.svg');
+        featureControl = true;
+    }
+}
+
+function companyMobile(){
+    if(companyControl){
+        document.querySelector('.m_company_op').style.display = 'flex';
+        document.querySelector('.m_company img').setAttribute('src', 'images/icon-arrow-up.svg');
+        companyControl = false;
+    }else{ 
+        document.querySelector('.m_company_op').style.display = 'none';
+        document.querySelector('.m_company img').setAttribute('src', 'images/icon-arrow-down.svg');
+        companyControl = true;
     }
 }
